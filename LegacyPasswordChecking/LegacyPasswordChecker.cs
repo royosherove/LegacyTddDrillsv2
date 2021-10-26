73e2a01b5044039637696c5d7551083c5d224b42
royo <royo>
2021-10-26 09:56:09 +0300

    Fix Checker

--- LegacyPasswordChecking/LegacyPasswordChecker.cs
+++ LegacyPasswordChecking/LegacyPasswordChecker.cs
@@ -7,10 +7,13 @@ namespace CalculatorLegacyDrills
     {
         public bool Check(string password)
         {
+            //length
             if (password.Length > 0 && password.Length < 17)
             {
-                if (Regex.IsMatch(password,"[a-z]") && Regex.IsMatch(password,"[A-Z]"))
+                //one lower case letter, one upper case letter
+                if (Regex.IsMatch(password,"[az]") && Regex.IsMatch(password,"[A-Z]"))
                 {
+                    //one number, one symbol
                     if (Regex.IsMatch(password,"0-9") 
                     && Regex.IsMatch(password, @"[!@#$%^&*()]"))
                     {
